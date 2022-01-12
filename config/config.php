096543b6052637f2e1a6583b0cfce5486dfe6011
matvejevs <matvejevs@MATVEJEVS-W>
2022-01-12 14:39:15 +0200

    another separating

---
+++ config/config.php
@@ -0,0 +1,5 @@
+<?php
+
+//path to controllers
+define('PathPrefix', '../controllers/');
+define('PathPostfix', 'Controller.php');
--- www/index.php
+++ www/index.php
@@ -1,30 +1,10 @@
 <?php
 
+include_once '../config/config.php';
+include_once '../library/mainFunctions.php';
 
 $controllerName = isset($_GET['controller']) ? ucfirst($_GET['controller']) : 'Index';
-echo 'Connection controller = ' . $controllerName . "</br>";
-
-
 $actionName = isset($_GET['action']) ? $_GET['action'] : 'index';
-echo 'Forming page function = '. $actionName . "</br>";
 
 
-include_once '../controllers/' . $controllerName . 'Controller.php';
-
-
-
-$function = $actionName . 'Action';
-echo 'Full function name = ' . $function . "</br>";
-
-
-
-
-//path to controllers
-define('PathPrefix', '../controllers/');
-define('PathPostfix', 'Controller.php');
-
-function loadPage($controllerName, $actionName = 'index'){
-	include_once PathPrefix . $controllerName . PathPostfix;
-	$function = $actionName . 'Action';
-    $function();
-}
+loadPage($controllerName, $actionName);
