897f2f90d0c29dd4d0b296133c701c47838292c8
matvejevs <matvejevs@MATVEJEVS-W>
2022-01-20 15:16:58 +0200

    fixes

--- tmp/smarty/templates_c/61abff1a202bfd988694c5f265f238e81a323570_0.file.leftcolumn.tpl.php
+++ tmp/smarty/templates_c/61abff1a202bfd988694c5f265f238e81a323570_0.file.leftcolumn.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.0.3, created on 2022-01-20 12:00:15
+/* Smarty version 4.0.3, created on 2022-01-20 14:12:43
   from 'C:\xampp\htdocs\shop_learn\views\default\leftcolumn.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.0.3',
-  'unifunc' => 'content_61e940bf6b14e4_95901440',
+  'unifunc' => 'content_61e95fcbb64cc2_74806149',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '61abff1a202bfd988694c5f265f238e81a323570' => 
     array (
       0 => 'C:\\xampp\\htdocs\\shop_learn\\views\\default\\leftcolumn.tpl',
-      1 => 1642676414,
+      1 => 1642684362,
       2 => 'file',
     ),
   ),
@@ -20,19 +20,20 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_61e940bf6b14e4_95901440 (Smarty_Internal_Template $_smarty_tpl) {
-?> <div id="leftColumn">
+function content_61e95fcbb64cc2_74806149 (Smarty_Internal_Template $_smarty_tpl) {
+?><div id="leftColumn">
 
     <div id="leftMenu">
         <div class="menuCaption">Menu:</div>
-<?php
+        <?php
 $_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['rsCategories']->value, 'item');
 $_smarty_tpl->tpl_vars['item']->do_else = true;
 if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['item']->value) {
 $_smarty_tpl->tpl_vars['item']->do_else = false;
 ?>
-    <a href="#"><?php echo $_smarty_tpl->tpl_vars['item']->value['name'];?>
-</a><br />
+            <a href="#"><?php echo $_smarty_tpl->tpl_vars['item']->value['name'];?>
+</a>
+            <br/>
         <?php
 }
 $_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
--- views/default/leftcolumn.tpl
+++ views/default/leftcolumn.tpl
@@ -1,9 +1,10 @@
- <div id="leftColumn">
+<div id="leftColumn">
 
     <div id="leftMenu">
         <div class="menuCaption">Menu:</div>
-{foreach $rsCategories as $item}
-    <a href="#">{$item['name']}</a><br />
+        {foreach $rsCategories as $item}
+            <a href="#">{$item['name']}</a>
+            <br/>
         {/foreach}
     </div>
 </div>
